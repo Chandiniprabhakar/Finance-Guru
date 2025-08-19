80ba4ff500c9fc896f8bc3da9bf990b1293a7ef8
Chandiniprabhakar <Chandiniprabahar@gmail.com>
2025-08-19 09:28:21 +0530

    final

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -6,23 +6,20 @@ export default function Page() {
     <section className="bg-white">
       <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
         <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-          <img
-            alt=""
+          <Image
+            alt="building"
             src="/building.jpeg"
-            className="absolute inset-0 h-full w-full object-cover opacity-80"
+            fill
+            className="absolute inset-0 object-cover opacity-80"
           />
         </section>
 
         <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
           <div className="max-w-xl lg:max-w-3xl">
             <div className="relative -mt-16 block lg:hidden">
-              <a
-                className="inline-flex size-16 items-center justify-center rounded-full bg-white text-[#FF9736] sm:size-20"
-                href="#"
-              >
-                <span className="sr-only">Home</span>
-                <Image src='/building.jpeg' alt="sideImage" fill />
-              </a>
+              <div className="relative w-16 h-16 sm:w-20 sm:h-20">
+                <Image src="/building.jpeg" alt="sideImage" fill className="rounded-full" />
+              </div>
 
               <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
                 Finance-Guru welcomes you
